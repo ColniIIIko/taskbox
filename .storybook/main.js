dfc81047e3ce308aa245dedac8e5c6d692313c1d
ColniIIIko <isavitskiy12@gmail.com>
2023-03-18 19:38:06 +0300

    taskbox

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+module.exports = {
+    stories: ["../src/components/**/*.stories.js"],
+    staticDirs: ["../public"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/preset-create-react-app",
+        "@storybook/addon-interactions",
+        "@storybook/addon-a11y",
+    ],
+    framework: {
+        name: "@storybook/react-webpack5",
+        options: {},
+    },
+    core: {
+        builder: "@storybook/builder-webpack5",
+    },
+    features: {
+        interactionsDebugger: true,
+    },
+};
---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+import { initialize, mswDecorator } from "msw-storybook-addon";
+import "../src/index.css";
+
+initialize();
+
+export const decorators = [mswDecorator];
+
+export const parameters = {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
+};
